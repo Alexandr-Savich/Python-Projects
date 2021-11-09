de224da30e426e5cbe4053d896bb742d105bdca7
Alexandr-Savich <71523798+Alexandr-Savich@users.noreply.github.com>
2021-11-09 16:17:17 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,112 +1,116 @@
-import csv
-from collections import defaultdict
+
+import random
+import abc
+import json
 
 
-def print_dict(d, indent=0) -> None:
-    """Печатает словарь в читабельном виде"""
-    for key, value in d.items():
-        print('\t' * indent + str(key))
-        if isinstance(value, dict):
-            print_dict(value, indent + 1)
+class AnimeMon(abc.ABC):
+    @abc.abstractmethod
+    def inc_exp(self, value: int):
+        raise IndexError
+
+
+class BasePokemon:
+    def __str__(self):
+        return f'{self.name}/{self.poketype}'
+
+
+class EmodjiMixin:
+    emodji = {
+        'grass': '🌿',
+        'fire': '🔥',
+        'water': '🌊',
+        'electric': '⚡',
+    }
+
+    def __str__(self):
+        text_pocketype = super().__str__()
+        return text_pocketype.replace(self.poketype, self.emodji[self.poketype])
+
+
+class Pokemon(EmodjiMixin, BasePokemon, AnimeMon):
+    def __init__(self, name: str, poketype: str):
+        self.name = name
+        self.poketype = poketype
+        self.exp = 0
+
+    def inc_exp(self, exp: int):
+        self.exp += exp
+
+
+def train(mon: AnimeMon):
+    step_size, level_size = 10, 100
+    sparring_qty = (level_size - mon.exp % level_size) // step_size
+    for i in range(sparring_qty):
+        win = random.choice([True, False])
+        if win:
+            mon.inc_exp(step_size)
+
+
+class Digimon(AnimeMon):
+    def __init__(self, name: str):
+        self.name = name
+        self.exp = 0
+
+    def inc_exp(self, value: int):
+        self.exp += value * 8
+
+
+class ColorizeMixin:
+    def color_print(self, text: str, repr_color_code: int):
+        return f'\033[{repr_color_code}m{text}'
+
+
+class Advert(ColorizeMixin):
+    repr_color_code = 33
+
+    @property
+    def price(self):
+        if '_Advert__price' in self.__dict__:
+            return self.__price
         else:
-            print('\t' * (indent + 1) + str(value))
+            return 0
 
+    def __transform(self, d: dict):
+        for key, value in d.items():
 
-def read_file() -> list:
-    """Читает таблицу из файла и возвращает лист из строк"""
-    with open('Corp_Summary.csv', newline='', encoding="utf8") as file:
-        reader = csv.DictReader(file, delimiter=';')
-        data = []
-        for i in reader:
-            data.append(i)
-    return data
+            if isinstance(value, dict):
+                self.__dict__[key] = Advert(value)
+            else:
+                if key == 'price':
+                    if value < 0:
+                        print('Price must be >0')
+                        raise ValueError
+                    else:
+                        self.__price = value
+                    continue
+                self.__dict__[key] = value
 
+    def __init__(self, ad: dict):
+        self.__transform(ad)
 
-def get_departments_structure(data: list) -> None:
-    """Выполняет первый запрос (создает словарь из депортаментов разбитых на отделы с числом сотрудников в
-    отделе) """
-    departments = {}
-    for row in data:
-        key = row['Департамент']
-        departments[key] = defaultdict(int)
-
-    for row in data:
-        key = row['Департамент']
-        departments[key][row['Отдел']] += 1
-    print_dict(departments)
-
-
-def get_statistics(data: list) -> dict:
-    """Выполняет второй запрос (создает словарь словарей из департаментов с ЗП и прочим)"""
-    departments = {}
-    for row in data:
-        key = row['Департамент']
-        departments[key] = defaultdict(float)
-
-    for row in data:
-        key = row['Департамент']
-        departments[key]['Численность работников'] += 1
-
-        if int(departments[key]['Минимальная зарплата']) == 0:
-            departments[key]['Минимальная зарплата'] = float(row['Оклад'])
-        if departments[key]['Минимальная зарплата'] > float(row['Оклад']):
-            departments[key]['Минимальная зарплата'] = float(row['Оклад'])
-
-        if departments[key]['Максимальная зарплата'] < float(row['Оклад']):
-            departments[key]['Максимальная зарплата'] = float(row['Оклад'])
-
-        departments[key]['Всего'] += float(row['Оклад'])
-
-        departments[key]['Средняя зарплата'] = departments[key]['Всего'] / departments[key][
-            'Численность работников']
-
-    for i in departments.keys():
-        departments[i].pop('Всего')
-    return departments
-
-
-def print_csv_dict(departments: dict):
-    """Сохраняет departments.csv файл со статистикой по департаментам"""
-
-    with open('departments.csv', 'w', newline='', encoding="utf8") as file:
-        key = departments.keys()
-        fieldnames = ['Департамент']
-        fieldnames = fieldnames + (list(departments[list(key)[0]].keys()))
-
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
-        writer.writeheader()
-        for department in departments:
-            d = {key: value for key, value in departments[department].items()}
-            d['Департамент'] = department
-            writer.writerow(d)
-
-    print('Файл departments.csv успешно сохранён!\n')
-
-
-def save_csv(departments: dict) -> dict:
-    """Запускает печать в файл словаря"""
-    print_csv_dict(departments)
-
-
-def get_inquiry(data: list) -> None:
-    """Получает запрос с клавиатуры"""
-
-    menu = ''
-    options = {'1': get_departments_structure,
-               '2': get_statistics,
-               '3': get_statistics}
-    print('Выберите:{}/{}/{}'.format(*options))
-    menu = input()
-    while menu not in options:
-        print('Выберите:{}/{}/{}'.format(*options))
-        menu = input()
-    dep = options[menu](data)
-    if menu == '2':
-        print_dict(dep)
-    if menu == '3':
-        save_csv(dep)
+    def __repr__(self):
+        return self.color_print(f'{self.title} | {self.price} ₽', self.repr_color_code)
 
 
 if __name__ == '__main__':
-    data = read_file()
-    get_inquiry(data)
+    pikachu = Pokemon(name='Pikachu', poketype='electric')
+    train(pikachu)
+    print(f'Pikachu:{pikachu.exp}')
+    agumon = Digimon(name='Agumon')
+    train(agumon)
+    print(f'Agumon:{agumon.exp}')
+
+    lesson_str = """{
+    "title": "python",
+    "price": 2128506,
+    "location": {
+    "address": "город Москва, Лесная, 7",
+    "metro_stations": ["Белорусская"]
+    }
+    }"""
+    lesson = json.loads(lesson_str)
+    print(lesson)
+
+    ad = Advert(lesson)
+    print(ad)
