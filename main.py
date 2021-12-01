c1788c38ca2ee094f6af4ab5e4f11147e65ee7fb
Alexandr-Savich <71523798+Alexandr-Savich@users.noreply.github.com>
2021-12-01 17:02:20 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,72 @@
+import doctest
+import pytest
+
+"""Morse Code Translator"""
+
+LETTER_TO_MORSE = {
+    'A': '.-', 'B': '-...', 'C': '-.-.',
+    'D': '-..', 'E': '.', 'F': '..-.',
+    'G': '--.', 'H': '....', 'I': '..',
+    'J': '.---', 'K': '-.-', 'L': '.-..',
+    'M': '--', 'N': '-.', 'O': '---',
+    'P': '.--.', 'Q': '--.-', 'R': '.-.',
+    'S': '...', 'T': '-', 'U': '..-',
+    'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..', '1': '.----',
+    '2': '..---', '3': '...--', '4': '....-',
+    '5': '.....', '6': '-....', '7': '--...',
+    '8': '---..', '9': '----.', '0': '-----',
+    ', ': '--..--', '.': '.-.-.-', '?': '..--..',
+    '/': '-..-.', '-': '-....-', '(': '-.--.', ')': '-.--.-',
+    ' ': ' '
+}
+
+MORSE_TO_LETTER = {
+    morse: letter
+    for letter, morse in LETTER_TO_MORSE.items()
+}
+
+
+def encode(message: str) -> str:
+    """
+    Кодирует строку в соответсвие с таблицей азбуки Морзе
+    >>> encode('SOS')
+    '... --- ...'
+    >>> encode ('MSU')
+     '-- ... ..-'
+    >>> encode ('MIT IS BETTER THAN HSE') # doctest: +ELLIPSIS
+    '-- .. - ... .... ... .'
+    >>> encode('help')
+    Traceback (most recent call last):
+    ...
+    KeyError: 'h'
+    """
+    encoded_signs = [
+        LETTER_TO_MORSE[letter] for letter in message
+    ]
+
+    return ' '.join(encoded_signs)
+
+
+def decode(morse_message: str) -> str:
+    """
+    Декодирует строку из азбуки Морзе в английский
+    """
+    decoded_letters = [
+        MORSE_TO_LETTER[letter] for letter in morse_message.split()
+    ]
+
+    return ''.join(decoded_letters)
+
+
+@pytest.mark.parametrize('code,message', [
+    ('... --- ...', 'SOS'),
+    ('.- -... -.-.', 'ABC'),
+    ('.-.. --- -. --.   . ...- . -. .. -. --.', 'LONG EVENING')
+])
+def test_decode(code, message):
+    assert decode(code) == message, f'expected {message}, got {decode(code)}'
+
+
+if __name__ == '__main__':
+    doctest.testmod()
